4aa01b23f655a61b1d55a7c1ec23fdaf33399abd
bhavya <bhavya@gmail.com>
2023-10-16 13:39:14 +0530

    first

---
+++ sample/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Hy Mohan I created the sample page </h2><br/>
+        <h3>Have You Observed that??</h3>
+        <img src={logo} className="App-logo" alt="logo" />
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
