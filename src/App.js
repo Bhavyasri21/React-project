395ee0d2c2a63dead638f61a4fb7d0ddb223a1db
mohankrishna007 <70874594+mohankrishna007@users.noreply.github.com>
2025-06-29 00:58:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
       <header className="App-header">
         <h2>Hy Mohan I created the sample page </h2><br/>
         <h3>Have You Observed that??</h3>
-        <h3> Yes, I saw, Super pandi </h3>
         <img src={logo} className="App-logo" alt="logo" />
        
       </header>
